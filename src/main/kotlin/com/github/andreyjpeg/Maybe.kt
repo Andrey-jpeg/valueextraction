a9084a5c3c50c63ab93012cb9b0e99f1afa3bb17
Casper Andresen <casper.andresen@cardlay.com>
2024-10-11 13:16:44 +0200

    remove unnecessary functions

--- src/main/kotlin/com/github/andreyjpeg/Maybe.kt
+++ src/main/kotlin/com/github/andreyjpeg/Maybe.kt
@@ -12,17 +12,3 @@ data class Some<out T>(val value: T) : Maybe<T>() {
 }
 
 data object None : Maybe<Nothing>()
-
-inline fun <T, R> Maybe<T>.flatMap(block: (T) -> Maybe<R>): Maybe<R> = when (this) {
-    is Some -> block(value)
-    is None -> this
-}
-
-inline fun <T, R> Maybe<T>.map(block: (T) -> R): Maybe<R> = flatMap { Some(block(it)) }
-
-fun <T> Maybe<Maybe<T>>.flatten(): Maybe<T> = flatMap { it }
-
-fun <T> Maybe<T>.toList(): List<T> = when (this) {
-    is Some -> listOf(value)
-    is None -> emptyList()
-}
